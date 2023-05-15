0cd136f3133b0879033c6c29bf825f9422762ae4
accsAkn <f22da037@chuo.ac.jp>
2023-05-15 13:59:17 +0900

    カメラ設定

--- Assets/Script/Camera_scr.cs
+++ Assets/Script/Camera_scr.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 //カメラの位置変更スクリプト
 public class Camera_scr : MonoBehaviour
 {
-    [SerializeField]private float camSpeed = 1.0f;
+    [SerializeField]private GameObject player;
     void Update()
     {
         //現在の位置を取得
@@ -12,8 +12,9 @@ public class Camera_scr : MonoBehaviour
 
         //取得した位置をVector3で代入
         Vector3 camPos = camTrans.position;
+        Vector3 plyPos = player.transform.position;
         //位置の変更
-        camPos.z = camPos.z + camSpeed * 0.01f;
+        camPos.z = plyPos.z - 7;
         //変更した位置を代入
         camTrans.position = camPos;
     }
